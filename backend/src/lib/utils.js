cfd82098b004335c30b89610224e5995ec76f88a
khaieelim <s4058427@student.rmit.edu.au>
2026-01-19 14:52:25 +0800

    Implemented login and logout functionality

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -15,7 +15,7 @@ export const generateToken = (userId, res) => {
         maxAge: 7*24*60*60*1000, // 7 days in Milliseconds
         httpOnly:true, // prevent XSS attacks(cross-site scripting)
         sameSite: "strict", // prevent CSRF attacks
-        secure: ENV.NODE_ENV === "development" ? false : true,
+        secure: ENV.NODE_ENV !== "development",
     });
 
     return token;
