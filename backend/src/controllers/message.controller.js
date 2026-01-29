96ccff9bd92f00218cb6bf0e9ff898ee16a05b97
khaieelim <s4058427@student.rmit.edu.au>
2026-01-29 17:42:19 +0800

    message routes optimized

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -39,6 +39,17 @@ export const sendMessage = async (req, res) => {
         const { id: receiverId } = req.params;
         const senderId = req.user._id;
 
+        if (!text && !message) {
+            return res.status(400).json({message: "Text or image is required."});
+        }
+        if (sender.Id.equals(receiverId)) {
+            return res.status(400).json({message: "Cannot send messages to yourself."});
+        }
+        const receiverExists = await User.exists({_id: receiverId});
+        if (!receiverExists) {
+            return res.status(404).json({message: "Receiver not found."});
+        }
+
         let imageUrl;
         if (image) {
             // upload base 64 image to cloudinary
