cfc0a5164fc1048f6098ec8bd9fc7637cb0836f8
khaieelim <s4058427@student.rmit.edu.au>
2026-01-31 22:44:51 +0800

    signup implemented in frontend

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,11 +2,13 @@ import express from "express"
 import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
 import path from "path";
+import cors from "cors";
 
 import authRoutes from "./routes/auth.route.js"
 import messageRoutes from "./routes/message.route.js"
 import {connectDB} from "./lib/db.js";
 import {ENV} from "./lib/env.js";
+
 
 dotenv.config();
 
@@ -16,6 +18,7 @@ const __dirname = path.resolve();
 const PORT = ENV.PORT|| 3000;
 
 app.use(express.json());
+app.use(cors({origin:ENV.CLIENT_URL, credentials: true}));
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
